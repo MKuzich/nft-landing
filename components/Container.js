6103cc71813c9e05a360a999c86a7bbfa64681bb
Mykhailo Kuzich <timmkedth@gmail.com>
2023-01-01 17:32:36 +0200

    added header responsive styles

--- components/Container.js
+++ components/Container.js
@@ -1,5 +1,7 @@
 const Container = ({ children }) => (
-  <div className="container mx-auto px-52 relative">{children}</div>
+  <div className="container mx-auto px-7 desktop:px-52 relative">
+    {children}
+  </div>
 );
 
 export default Container;
--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import logo from "../public/logo.png";
 import dark from "../public/dark.png";
 import light from "../public/light.png";
 import english from "../public/english.png";
+import title from "../public/title.png";
 import NavBar from "./NavBar";
 import Container from "./Container";
 
@@ -23,12 +24,27 @@ const Header = () => {
     <header className="pt-7 bg-slate-900">
       <Container>
         <div className="flex items-center justify-between">
-          <div className="flex gap-16 items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 tablet:hidden"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+          <Image src={title} alt="Company title" width={120} height={22} />
+          <div className="tablet:flex gap-16 items-center hidden">
             <Image src={logo} alt="Logo" width={45} height={45} />
             <NavBar />
           </div>
           <div className="flex gap-7 items-center">
-            <button type="button">
+            <button type="button" className="hidden tablet:block">
               <Image src={english} alt="language" width={33} height={24} />
             </button>
             <button
@@ -43,7 +59,7 @@ const Header = () => {
                 height={24}
               />
             </button>
-            <div className="flex gap-4">
+            <div className="hidden tablet:flex gap-4">
               <button className="header-btn">Sign in</button>
               <button className="header-gradient-btn">Register</button>
             </div>
